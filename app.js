eabfeff882b22ead30bbc21bc360fa10ec6d7a20
Harzminers <darian@sweetsforsweets.de>
2023-04-22 19:36:44 +0200

    Finished Guessing Game and started css dom change.

--- app.js
+++ app.js
@@ -85,5 +85,125 @@
 
 // printForecast(temperatureForcast);
 //________________________________________________________________________
+const maxGuessingNumber = 100;
+const minGuessingNumber = 0;
 
-console.log(document.querySelector(`.numResponseUI`).textContent);
+let gameHasEnded = false;
+
+let currentScore = maxGuessingNumber;
+let highScore = 0;
+
+let mysteryNumber = 0;
+
+const today = new Date();
+mysteryNumber = SetNewMysteryNumber();
+InitNewGame();
+
+function AppendToLog(string) {
+  // document.querySelector(`.guessing-log`).textContent +=
+  //   "(" +
+  //   today.getHours() +
+  //   " : " +
+  //   today.getMinutes() +
+  //   " : " +
+  //   today.getSeconds() +
+  //   ")" +
+  //   " : " +
+  //   string +
+  //   "\n";
+}
+
+function SetNewMysteryNumber() {
+  mysteryNumber =
+    Math.round(
+      Math.random() * (maxGuessingNumber - minGuessingNumber) +
+        minGuessingNumber
+    ) + 1;
+  return mysteryNumber;
+}
+// set initial values
+function InitNewGame() {
+  AppendToLog(`A new round has been started!`);
+  gameHasEnded = false;
+  SetNewMysteryNumber();
+  currentScore = maxGuessingNumber;
+  document.querySelector(`.mysteryNumber`).textContent = "?";
+  document.querySelector(
+    `.current-score`
+  ).textContent = `Current Score: ${maxGuessingNumber}`;
+  document.querySelector(`.high-score`).textContent = `Highscore: ${highScore}`;
+  document.querySelector(
+    `.page-subtitle`
+  ).textContent = `What number between ${minGuessingNumber} and ${maxGuessingNumber} am I thinking of?`;
+  document.querySelector(`.numberInput`).value = ``;
+  document.querySelector(`.numResponseUI`).textContent = "Start guessing...";
+  document.querySelector(`body`).style.backgroundColor = `whiteSmoke`;
+}
+
+document.querySelector(`.checkBtn`).addEventListener(`click`, function () {
+  console.log(document.querySelector(`.numberInput`).value);
+  guessValue = Number(document.querySelector(`.numberInput`).value);
+
+  if (gameHasEnded) return;
+  if (currentScore <= 1) {
+    document.querySelector(`.numResponseUI`).textContent = `You lost the Game.`;
+    gameHasEnded = true;
+    currentScore--;
+    document.querySelector(
+      `.current-score`
+    ).textContent = `Current Score: ${currentScore}`;
+    AppendToLog(`You have lost the Game!`);
+    return;
+  }
+  if (
+    (!guessValue && guessValue !== 0) ||
+    typeof guessValue !== "number" ||
+    guessValue > maxGuessingNumber ||
+    guessValue < minGuessingNumber
+  ) {
+    document.querySelector(`.numResponseUI`).textContent = "Invalid Input!";
+    AppendToLog(`Entered an invalid Input (${guessValue})!`);
+    return;
+  }
+
+  const responseMessage =
+    guessValue > mysteryNumber
+      ? "Too high!"
+      : guessValue < mysteryNumber
+      ? "Too low!"
+      : "Correct Number!";
+
+  document.querySelector(`.numResponseUI`).textContent = responseMessage;
+
+  if (guessValue === mysteryNumber) {
+    document.querySelector(
+      `.high-score`
+    ).textContent = `Highscore: ${currentScore}`;
+    document.querySelector(`.mysteryNumber`).textContent = mysteryNumber;
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+    highScore = currentScore;
+    gameHasEnded = true;
+    AppendToLog(`Guessed ${guessValue}. This is the correct Number!`);
+    AppendToLog(`Round has ended. Start a new round to play again.`);
+  } else {
+    currentScore--;
+    document.querySelector(
+      `.current-score`
+    ).textContent = `Current Score: ${currentScore}`;
+    AppendToLog(`Guessed ${guessValue}. This is ${responseMessage}.`);
+  }
+});
+
+document.querySelector(`.numberInput`).addEventListener(`change`, function () {
+  value = Number(document.querySelector(`.numberInput`).value);
+  document.querySelector(`.numberInput`).value =
+    value > maxGuessingNumber
+      ? maxGuessingNumber
+      : value < minGuessingNumber
+      ? minGuessingNumber
+      : value;
+});
+
+document.querySelector(`.restart-btn`).addEventListener(`click`, function () {
+  InitNewGame();
+});
